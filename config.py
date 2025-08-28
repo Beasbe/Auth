32c6eae7e4a65476f6471157054593ea0cb7399e
Alex Semyonkin <99828026+Beasbe@users.noreply.github.com>
2025-08-28 12:10:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-GITHUB_CLIENT_ID = "Iv23lidjRwLsXZS4RZvH"
-GITHUB_CLIENT_SECRET = "0fa2c24329abe51a99d24a61c84ae1b6b799fd97"
+GITHUB_CLIENT_ID = "your_client_id"
+GITHUB_CLIENT_SECRET = "your_client_secret"
 FRONTEND_URL = "http://localhost:5173"
 REDIRECT_URI = "http://localhost:8000/auth/github/callback"
 
 GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize"
 GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
 GITHUB_USERINFO_URL = "https://api.github.com/user"
+
